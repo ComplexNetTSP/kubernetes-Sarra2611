37e07c49432da217733ff68cae428468f5fcbc87
bellator2611 <sarra.b207@gmail.com>
2025-01-27 11:01:49 +0100

    challenge13

---
+++ challenge 13/webdb/app.py
@@ -0,0 +1,34 @@
+# app.py
+from flask import Flask, request
+from datetime import datetime
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient("mongodb://mongodb:27017/")
+db = client["flaskdb"]
+logs = db["logs"]
+
+@app.route("/")
+def home():
+    client_ip = request.remote_addr
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logs.insert_one({"ip": client_ip, "date": current_date})
+
+    # Fetch the last 10 records
+    recent_logs = logs.find().sort("_id", -1).limit(10)
+    log_display = "<br>".join([f"IP: {log['ip']}, Date: {log['date']}" for log in recent_logs])
+    
+    
+    return f"""
+    <h1>Name: Sarra Barnoussi</h1>
+    <h2>Project Name: My Flask Docker App</h2>
+    <h3>Version: V2</h3>
+    <h4>Current Date: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</h4>
+    <h5>Last 10 Logs:</h5>
+    <p>{log_display}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ challenge 13/webnodb/app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    current_url = request.url 
+    return f"""
+    <h1>Name: Sarra Barnoussi</h1>
+    <h2>Project Name: My Flask Docker App</h2>
+    <h3>Version: V2</h3>
+    <h4>Server Hostname (Full URL): {current_url}</h4>
+    <h5>Current Date: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</h5>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
